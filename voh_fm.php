a68535a145e418468d31b06d5fa9bee21bd52a2c
Yuki Kikuchi <24319042+bclswl0827@users.noreply.github.com>
2022-05-08 19:24:33 +0800

    Add source

--- voh_fm.php
+++ voh_fm.php
@@ -1,7 +1,7 @@
 <?php
 
 // 设置缓存目录（存放 Token 和过期时间等）
-$tmpDir = './live_conf';
+$tmpDir = './live_log';
 
 // 用于保存数据
 function saveData($baseName, $liveToken, $liveExpiration, $saveDir) {
@@ -41,51 +41,64 @@ function parseUrl($liveUrl, $urlParam) {
     }
 }
 
-// 解析光华之声网站获取 HLS 地址
-function parseKhmusic() {
-    // 获取光华之声网页，并用正则提取地址
-    $khmusicUrl = 'https://audio.voh.com.tw/KwongWah/m3u8.aspx';
-    $webContent = str_replace('&amp;', '&', file_get_contents($khmusicUrl));
-    preg_match_all('/https:\/\/vohradiow-hichannel.cdn.hinet.net\/live\/RA000077\/playlist.m3u8\?token=.+&expires=\d+/i', $webContent, $regExp);
-    // 第一个即为直播地址，并替换 playlist.m3u8 为 chunklist.m3u8
-    return str_replace('playlist', 'chunklist', $regExp[0][0]);
-}
+// 汉声电台 API 获取 HLS 地址
+function parseVoh($stationNum) {
+    // 构造请求体参数
+    $postData = array('http' =>
+        array(
+            'method' => 'POST',
+            'header' => 'Content-type: application/x-www-form-urlencoded; charset=UTF-8',
+            'content' => http_build_query(
+                array(
+                    'Type' => 'VideoDisplay',
+                    'PlayChannel' => $stationNum
+                )
+            )
+        )
+    );
 
+    $apiRes = file_get_contents(
+        'https://audio.voh.com.tw/API/ugC_ProgramHandle.ashx',
+        false,
+        stream_context_create($postData)
+    );
+    $jsonArray = json_decode($apiRes, true);
+    return $jsonArray['Url'] . '?token=' . $jsonArray['token'] . '&expires=' . $jsonArray['expires'];
+}
 
 // 检查文件夹是否存在
 if (!is_dir($tmpDir)) {
     // 若文件夹不存在，则先建立一个
-    mkdir($tmpDir, 0755, true);
-    // 获取 M3U8 地址
-    $m3u8Link = parseKhmusic();
+    mkdir($tmpDir, 0777, true);
+    // 获取汉声 FM 台 M3U8 地址，1 是 FM，2 是 AM
+    $m3u8Link = parseVoh(1);
     // 内容写入到文件
     $liveToken = parseUrl($m3u8Link, 'token');
     $liveExpiration = parseUrl($m3u8Link, 'expiration');
-    saveData('khmusic', $liveToken, $liveExpiration, $tmpDir);
+    saveData('voh_fm', $liveToken, $liveExpiration, $tmpDir);
 } else {
-    if (!file_exists($tmpDir . '/khmusic_token.txt') && !file_exists($tmpDir . '/khmusic_expiration.txt')) {
-        // 获取 M3U8 地址
-        $m3u8Link = parseKhmusic();
+    if (!file_exists($tmpDir . '/voh_fm_token.txt') && !file_exists($tmpDir . '/voh_fm_expiration.txt')) {
+        // 获取汉声 FM 台 M3U8 地址，1 是 FM，2 是 AM
+        $m3u8Link = parseVoh(1);
         // 内容写入到文件
         $liveToken = parseUrl($m3u8Link, 'token');
         $liveExpiration = parseUrl($m3u8Link, 'expiration');
-        saveData('khmusic', $liveToken, $liveExpiration, $tmpDir);
+        saveData('voh_fm', $liveToken, $liveExpiration, $tmpDir);
     } else {
         // 检查 Token 是否过期，未过期则直接读取原有的 Token
-        if (!validTime(readData($tmpDir, 'khmusic', 'expiration'))) {
-            // 重新获取 M3U8 地址
-            $m3u8Link = parseKhmusic();
+        if (!validTime(readData($tmpDir, 'voh_fm', 'expiration'))) {
+            // 重新获取 M3U8 地址，1 是 FM，2 是 AM
+            $m3u8Link = parseVoh(1);
             // 内容写入到文件
             $liveToken = parseUrl($m3u8Link, 'token');
             $liveExpiration = parseUrl($m3u8Link, 'expiration');
-            saveData('khmusic', $liveToken, $liveExpiration, $tmpDir);
+            saveData('voh_fm', $liveToken, $liveExpiration, $tmpDir);
         } else {
             // 从文件读取原有 Token
-            $liveToken = readData($tmpDir, 'khmusic', 'token');
-            $m3u8Link = 'https://vohradiow-hichannel.cdn.hinet.net/live/RA000077/chunklist.m3u8?token=' . $liveToken . '&expires=' . readData($tmpDir, 'khmusic', 'expiration');
+            $liveToken = readData($tmpDir, 'voh_fm', 'token');
+            $m3u8Link = 'https://vohradiow-hichannel.cdn.hinet.net/live/RA000076/chunklist.m3u8?token=' . $liveToken . '&expires=' . readData($tmpDir, 'voh_fm', 'expiration');
         }
     }
-
 }
 
 // 替换 TS 分片文件名
@@ -93,6 +106,6 @@ $streamContents = file_get_contents($m3u8Link);
 
 // 返回 M3U8 内容
 header('Content-Type: application/vnd.apple.mpegurl');
-echo str_replace('media_', 'https://vohradiow-hichannel.cdn.hinet.net/live/RA000077/media_', $streamContents);
+echo str_replace('media_', 'https://vohradiow-hichannel.cdn.hinet.net/live/RA000076/media_', $streamContents);
 
 ?>
